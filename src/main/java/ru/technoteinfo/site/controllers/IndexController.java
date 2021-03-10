7d267832c422afd252ea1aec29753cb84bbe45cf
Igor <sidorenko.iv@mail.ru>
2021-03-10 22:20:02 +0300

    Структура

--- src/main/java/ru/technoteinfo/site/controllers/IndexController.java
+++ src/main/java/ru/technoteinfo/site/controllers/IndexController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class IndexController {
 
     @GetMapping
     @ResponseBody
-    public String index(){
-        return "data";
+    public Map<Integer, String> index(){
+        Map<Integer, String> list = new HashMap<>();
+        for (int i=0; i<=10; i++){
+            list.put(i, "Строка: "+i);
+        }
+        return list;
     }
 }
