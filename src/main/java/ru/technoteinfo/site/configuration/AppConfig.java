02dd148d3fa0f0131e3dbe3842ed0bbca4a3c3f5
Igor <sidorenko.iv@mail.ru>
2021-03-10 22:08:38 +0300

    first commit

---
+++ src/main/java/ru/technoteinfo/site/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.technoteinfo.site.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.technoteinfo.site")
+public class AppConfig {
+}
---
+++ src/main/java/ru/technoteinfo/site/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package ru.technoteinfo.site.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    @ResponseBody
+    public String index(){
+        return "data";
+    }
+}
