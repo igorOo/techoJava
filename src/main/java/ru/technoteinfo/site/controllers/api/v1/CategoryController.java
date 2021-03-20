c16842cc282e2c8851780c6b4ead2ff2698630d0
igor <mail@mail.ru>
2021-03-20 18:56:13 +0300

    category controller

---
+++ src/main/java/ru/technoteinfo/site/controllers/api/v1/CategoryController.java
@@ -0,0 +1,12 @@
+package ru.technoteinfo.site.controllers.api.v1;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value="/category")
+public class CategoryController {
+
+}
