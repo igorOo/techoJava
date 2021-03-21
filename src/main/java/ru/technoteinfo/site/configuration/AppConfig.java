8373ee10ab749ff2647d3dbc7610da3c041672c1
igor <mail@mail.ru>
2021-03-21 22:52:41 +0300

    вывел свои настройки в отдельный файл с заменой url

--- src/main/java/ru/technoteinfo/site/configuration/AppConfig.java
+++ src/main/java/ru/technoteinfo/site/configuration/AppConfig.java
@@ -2,8 +2,10 @@ package ru.technoteinfo.site.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("ru.technoteinfo.site")
+@PropertySource("classpath:site.properties")
 public class AppConfig {
 }
