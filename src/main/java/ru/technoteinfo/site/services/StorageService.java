6b1e583904a2b28ea807bafacf2c77db36e66d02
Igor <sidorenko.iv@mail.ru>
2021-05-16 23:23:47 +0300

    Галерея

--- src/main/java/ru/technoteinfo/site/services/StorageService.java
+++ src/main/java/ru/technoteinfo/site/services/StorageService.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.security.util.InMemoryResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import ru.technoteinfo.site.entities.Gallery;
 import ru.technoteinfo.site.repositories.GalleryRepository;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,6 +25,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,7 +48,7 @@ public class StorageService {
 
             BufferedImage resizedImage = resizeImage(file, resolut[0], resolut[1]);
 
-            Resource resource = new UrlResource(resizedImage.toString());
+            Resource resource = new UrlResource(file.toUri());
             if (resource.exists() || resource.isReadable()) {
                 return resource;
             }
@@ -56,10 +62,24 @@ public class StorageService {
         }
     }
 
-    public BufferedImage resizeImage(Path file, int width, int heigth){
-
+    //TODO::запихать в отдельный поток
+    public BufferedImage resizeImage(Path file, int width, int heigth) throws IOException {
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        tmpDir = tmpDir.replaceAll("\\\\", "/");
+        BufferedImage originalImage = ImageIO.read(file.toFile());
+        Image resizedImage = originalImage.getScaledInstance(width, heigth, Image.SCALE_SMOOTH);
+        BufferedImage resultImage = new BufferedImage(width, heigth, BufferedImage.TYPE_INT_ARGB);
+        resultImage.getGraphics().drawImage(resizedImage, 0,0,null);
+        return resultImage;
+//        String fileName = tmpDir+UUID.randomUUID().toString()+".jpg";
+//        if (ImageIO.write(resultImage, "jpg", new File(fileName))){
+//            return resultImage;
+//        }else{
+//            throw new IOException("Не удалось создать новый файл");
+//        }
     }
 
+    //TODO::запихать в отдельный поток
     public BufferedImage cropImage(Path file, int toWidth, int toHeigth) throws IOException {
         BufferedImage originalImage = ImageIO.read(file.toFile());
         int height = originalImage.getHeight();
