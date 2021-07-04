2fa09c6e9251de7cb29556b990e8e312c676ce1f
igor <mail@mail.ru>
2021-07-04 18:13:36 +0300

    checkout

--- src/main/java/ru/technoteinfo/site/configuration/AppConfig.java
+++ src/main/java/ru/technoteinfo/site/configuration/AppConfig.java
@@ -2,6 +2,7 @@ package ru.technoteinfo.site.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
